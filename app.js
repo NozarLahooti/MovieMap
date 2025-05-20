a5f08ed4caa8aff8effdf81e37df114e4df233aa
NozarLahooti <nozarlahooti@me.com>
2025-05-19 21:36:42 -0400

    Dynamic Button

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ function addMovie() {
     const movieName = document.getElementById('movieInput').value.trim();
     if (!movieName) return;  
     const list = document.getElementById('movieList');
-    list.innerHTML += `<li>${movieName}</li>`;
+    const li = document.createElement('li');
+    li.textContent = movieName;
+    list.appendChild(li);
     document.getElementById('movieInput').value = ''; 
   }
-  
+
+// Add Button Dynamically Using Function
+const button = document.querySelector('#addButton');
+const input = document.getElementById('movieInput');
+
+button.addEventListener('click', addMovie);
+
+input.addEventListener('keypress', event => {
+  if (event.key === 'Enter') {
+    addMovie();
+  }
+});
